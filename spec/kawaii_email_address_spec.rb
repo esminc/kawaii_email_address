790d42ad3f6f7f574215c1b87fc1e6847206ee33
moro <moronatural@gmail.com>
2013-07-09 20:04:59 +0900

    Add kawaii test

--- spec/kawaii_email_address_spec.rb
+++ spec/kawaii_email_address_spec.rb
@@ -25,6 +25,8 @@ describe KawaiiEmailAddress::Validator do
     it { should pass_validation_with 'mail..example@docomo.ne.jp' }
     it { should pass_validation_with 'example.@docomo.ne.jp' }
     it { should pass_validation_with '.example@docomo.ne.jp' }
+
+    it { should pass_validation_with 'u3u..chu.@docomo.ne.jp' }
   end
 
   context 'domain part' do
