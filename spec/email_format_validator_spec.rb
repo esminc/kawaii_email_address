f9be17296f0046238ff7e5253bd0fc3e76bfbcab
moro <moronatural@gmail.com>
2013-07-09 19:30:18 +0900

    Refactoring: restructure-rspec

--- spec/email_format_validator_spec.rb
+++ spec/email_format_validator_spec.rb
@@ -2,95 +2,38 @@ require 'email_format_validator/address'
 
 describe EmailFormatValidator::Address do
 
-  subject(:address) { EmailFormatValidator::Address.new(addr) }
-
-  context 'when a@example.com' do
-    let(:addr) { 'a@example.com' }
-    it { should be_valid }
+  RSpec::Matchers.define(:pass_validation_with) do |addr|
+    match do |validator_klass|
+      validator_klass.new(addr).valid?
+    end
   end
 
-  context 'when a@example.com' do
-    let(:addr) { 'a.!/==@example.com' }
-    it { should be_valid }
+  subject { EmailFormatValidator::Address }
+
+  it { should pass_validation_with 'a@example.com' }
+  it { should pass_validation_with 'a.!/==@example.com' }
+
+  it { should_not pass_validation_with 'a@bc@example.com' }
+  it { should_not pass_validation_with 'a..b@example.com' }
+  it { should_not pass_validation_with 'a.@example.com' }
+  it { should_not pass_validation_with '.aaa@example.com' }
+
+  it { should pass_validation_with '"a@a"@example.com' }
+  it { should pass_validation_with "'or'1'='1'--@example.com" }
+
+  context 'kawaii: aka.docomo' do
+    it { should pass_validation_with 'mail..example@docomo.ne.jp' }
+    it { should pass_validation_with 'example.@docomo.ne.jp' }
+    it { should pass_validation_with '.example@docomo.ne.jp' }
   end
 
-  context 'when a@example.com' do
-    let(:addr) { 'a@bc@example.com' }
-    it { should_not be_valid }
-  end
+  context 'domain part' do
+    it { should_not pass_validation_with 'example@c' }
+    it { should_not pass_validation_with 'example@-com.jp' }
+    it { should_not pass_validation_with 'example@com.jp-' }
+    it { should_not pass_validation_with 'example@com.j' }
 
-  context 'when a..@example.com' do
-    let(:addr) { 'a..b@example.com' }
-    it { should_not be_valid }
-  end
-
-  context 'when a.@example.com' do
-    let(:addr) { 'a.@example.com' }
-    it { should_not be_valid }
-  end
-
-  context 'when .aaa@example.com' do
-    let(:addr) { '.aaa@example.com' }
-    it { should_not be_valid }
-  end
-
-  context 'not valid case' do
-    let(:addr) { 'a@a@a' }
-    it { should_not be_valid }
-  end
-
-  context 'quotes @' do
-    let(:addr) { '"a@a"@example.com' }
-    it { should be_valid }
-  end
-
-  context 'complex symbols' do
-    let(:addr) { "'or'1'='1'--@example.com" }
-    it { should be_valid }
-  end
-
-  context 'has double dots' do
-    let(:addr) { 'mail..example@docomo.ne.jp' }
-    it { should be_valid }
-  end
-
-  context 'dot just before @' do
-    let(:addr) { 'example.@docomo.ne.jp' }
-    it { should be_valid }
-  end
-
-  context 'start with dot' do
-    let(:addr) { '.example@docomo.ne.jp' }
-    it { should be_valid }
-  end
-
-  context 'domain part too short' do
-    let(:addr) { 'example@c' }
-    it { should_not be_valid }
-  end
-
-  context 'domain part start with hyphen' do
-    let(:addr) { 'example@-com.jp' }
-    it { should_not be_valid }
-  end
-
-  context 'domain part end with hyphen' do
-    let(:addr) { 'example@com.jp-' }
-    it { should_not be_valid }
-  end
-
-  context 'domain last part too short' do
-    let(:addr) { 'example@com.j' }
-    it { should_not be_valid }
-  end
-
-  context 'domain part is valid domain literal' do
-    let(:addr) { 'example@[115.146.17.185]' }
-    it { should be_valid }
-  end
-
-  context 'domain part is invalid domain literal' do
-    let(:addr) { 'example@[115.146.1722]' }
-    it { should_not be_valid }
+    it { should     pass_validation_with 'example@[115.146.17.185]' }
+    it { should_not pass_validation_with 'example@[115.146.1722]' }
   end
 end
